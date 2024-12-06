3ebb9f1423590bec9826890502cd0d26a3383b66
Victor Nascimento <vicnp9@hotmail.com>
2024-12-05 21:07:00 -0300

    Update appsettings.

--- src/TechChallenge.Fase3.Infra/Utils/MensageriaBus.cs
+++ src/TechChallenge.Fase3.Infra/Utils/MensageriaBus.cs
@@ -1,5 +1,4 @@
 ﻿using EasyNetQ;
-using EasyNetQ.Topology;
 using Microsoft.Extensions.Configuration;
 using TechChallenge.Fase3.Domain.Contatos.Servicos.Interfaces;
 namespace TechChallenge.Fase3.Infra.Utils
@@ -18,17 +17,6 @@ namespace TechChallenge.Fase3.Infra.Utils
         {
             connectionString = configuration.GetConnectionString("rabbitmq")!;
             Bus = RabbitHutch.CreateBus(connectionString);
-            Exchange exchange = Bus.Advanced.ExchangeDeclare("techchallenge", ExchangeType.Direct, durable: true, autoDelete: false);
-
-            Queue queueInserir = Bus.Advanced.QueueDeclare("QueueInserir", durable: true, exclusive: false, autoDelete: false);
-            Queue queueRemover = Bus.Advanced.QueueDeclare("QueueRemover", durable: true, exclusive: false, autoDelete: false);
-            Queue queueEditar = Bus.Advanced.QueueDeclare("QueueEditar", durable: true, exclusive: false, autoDelete: false);
-
-            Bus.Advanced.Bind(exchange, queueInserir, "Contato.Inserir");
-            Bus.Advanced.Bind(exchange, queueRemover, "Contato.Remover");
-            Bus.Advanced.Bind(exchange, queueEditar, "Contato.Editar");
-
-            Console.WriteLine("BUS RABBIT CRIADO: " + connectionString);
         }
     }
 }
