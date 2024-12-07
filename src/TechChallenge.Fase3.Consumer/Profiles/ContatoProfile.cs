3d7e1570e75b24b91b66120f3f5b34f305dbac26
Victor Nascimento <vicnp9@hotmail.com>
2024-12-07 19:01:16 -0300

    Create ContatoProfile.cs

---
+++ src/TechChallenge.Fase3.Consumer/Profiles/ContatoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TechChallenge.Fase3.Domain.Contatos.Comandos;
+using TechChallenge.Fase3.Domain.Contatos.Entidades;
+
+namespace TechChallenge.Fase3.Consumer.Profiles
+{
+    public class ContatoProfile : Profile
+    {
+        public ContatoProfile()
+        {
+            CreateMap<Contato, ContatoComando>().ReverseMap();
+        }
+    }
+}
