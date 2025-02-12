f668fe842db284fd45a3d06cc6b429ba51d6be4d
jcoaio <jcoaio@gmail.com>
2025-02-11 21:31:42 -0300

    Update MensageriaBus.cs

--- src/TechChallenge.Fase3.Infra/Utils/MensageriaBus.cs
+++ src/TechChallenge.Fase3.Infra/Utils/MensageriaBus.cs
@@ -31,8 +31,11 @@ namespace TechChallenge.Fase3.Infra.Utils
             if (request == null)
                 throw new ArgumentNullException("Objeto nulo.");
 
-            ISendEndpoint endpoint = await _bus.GetSendEndpoint(new Uri($"queue:{nomeFila}-{Environment.MachineName}"));
-            await endpoint.Send(request);
+            await _bus.Publish(request, context => 
+            {
+                context.SetRoutingKey($"{nomeFila}-deployment-{Environment.MachineName}");
+            });
+
         }
 
         public string GetFilaInserir() => FilaInsert;
